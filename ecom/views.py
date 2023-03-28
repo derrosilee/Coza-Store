38ee48d0d108e2abb38f668e5abfc3d90436d06b
derrosilee <derrossilee@gmail.com>
2023-03-28 11:17:39 +0300

    Fixed home links

--- ecom/views.py
+++ ecom/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, template_name='BaseTemplates/home-03.html')
+    return render(request, template_name='BaseTemplates/index.html')
