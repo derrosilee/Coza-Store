6c3ecbb32843c5da232c62bfa78632e0cd71b0ee
derrosilee <derrossilee@gmail.com>
2023-03-28 13:26:46 +0300

    Addded 2nd navbar for products page

--- ecom/urls.py
+++ ecom/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('products/', views.products, name='products'),
+    path('test/', views.det, name='test')
 ]
