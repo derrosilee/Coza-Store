6e2e2599da6993b7f5ca6e78919947e3bcfacfec
derrosilee <derrossilee@gmail.com>
2023-03-30 07:00:28 +0300

    added about url

--- ecom/urls.py
+++ ecom/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('product-detail/', views.product_detail, name='product-detail'),
     path('track_order/', views.track_order, name='track_order'),
     path('cart/', views.cart, name='cart'),
+
+    path('about/', views.about, name='about'),
 ]
