f1020b49bb60b15f5f88275e0d5ad901ebe28f16
derrosilee <derrossilee@gmail.com>
2023-03-30 06:29:33 +0300

    added cart url

--- ecom/urls.py
+++ ecom/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='products'),
     path('product-detail/', views.product_detail, name='product-detail'),
-    path('track_order/', views.track_order, name='track_order')
+    path('track_order/', views.track_order, name='track_order'),
+    path('cart/', views.cart, name='cart'),
 ]
