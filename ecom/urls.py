419f0f69769c1ed4c94ce3b1775e632c2043e8d6
derrosilee <derrossilee@gmail.com>
2023-03-30 13:44:41 +0300

    added register page

--- ecom/urls.py
+++ ecom/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login, name='login'),
-    path('register/', views.login, name='register'),
+    path('register/', views.register, name='register'),
     path('products/', views.products, name='products'),
     path('product-detail/', views.product_detail, name='product-detail'),
     path('track_order/', views.track_order, name='track_order'),
