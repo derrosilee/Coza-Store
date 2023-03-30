6460b1f2f516fe1591b5cbe90c2a5aac210626b4
derrosilee <derrossilee@gmail.com>
2023-03-30 06:09:47 +0300

    Added Track Order url

--- ecom/urls.py
+++ ecom/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='products'),
-    path('product-detail/', views.product_detail, name='product-detail')
+    path('product-detail/', views.product_detail, name='product-detail'),
+    path('track_order/', views.track_order, name='track_order')
 ]
